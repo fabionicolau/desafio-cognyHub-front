7492b9c324d83551dac2b9eac6bb55ed547dbab5
fabionicolau <fabionicolaujr@gmail.com>
2023-03-03 19:25:03 -0300

    chore: melhora configuração do banco de dados
    - adiciona variáveis de ambiente às credenciais do firebase

---
+++ web/src/database/firebaseConfig.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+ } from "../env";
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
